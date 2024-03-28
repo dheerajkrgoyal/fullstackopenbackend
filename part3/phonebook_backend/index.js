876fe1eeb6614afe4c59291b380d301000297d89
dheerajkrgoyal <dheerajkrgoyal@gmail.com>
2024-03-28 19:37:56 +0530

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -8,6 +8,7 @@ morgan.token('request-body', function getId (req) {
     return JSON.stringify(req.body)
 })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
